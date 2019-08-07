f223af3c737a62c231cbe8b88e85d21fda8d985b
YanHao <yanhao@imdada.cn>
2019-08-07 18:13:11 +0800

    proj init

---
+++ src/main/java/org/carony/deebeebe/deebeebeparent/DeebeebeParentApplication.java
@@ -0,0 +1,13 @@
+package org.carony.deebeebe.deebeebeparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeebeebeParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeebeebeParentApplication.class, args);
+    }
+
+}
